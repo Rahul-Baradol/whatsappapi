7fe33caa3bb815685bd91aa0e97a578498afa924
rahul <rahul.baradol.14@gmail.com>
2024-02-23 16:31:23 +0530

    try

--- routes/post.js
+++ routes/post.js
@@ -7,18 +7,14 @@ const goodBoyUrl = 'https://images.unsplash.com/photo-1518717758536-85ae29035b6d
 router.post('/', async (req, res, next) => {
   const { body } = req;
 
-  console.log(req.headers);
-  console.log(req.body);
-  // console.log(req);
-  // let d = req.parser['4']()
-  // console.log(d);
+  console.log(body);
 
-  // // if (body.NumMedia > 0) {
-    // message = new MessagingResponse().message("Thanks for the image! Here's one for you!");
-  //   message.media(goodBoyUrl);
-  // } else {
+  if (body.NumMedia > 0) {
+    message = new MessagingResponse().message("Thanks for the image! Here's one for you!");
+    message.media(goodBoyUrl);
+  } else {
     message = new MessagingResponse().message('Send us an image!');
-  // }
+  }
 
   res.set('Content-Type', 'text/xml');
   res.send(message.toString()).status(200);
