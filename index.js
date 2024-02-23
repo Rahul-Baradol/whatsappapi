0821307186ff204908033fd5423f5101998c067a
rahul <rahul.baradol.14@gmail.com>
2024-02-23 16:28:31 +0530

    try

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors')
 const app = express();
 const postRouter = require('./routes/post');
+const bodyParser = require('body-parser');
 
 
 // Middleware to parse JSON bodies
